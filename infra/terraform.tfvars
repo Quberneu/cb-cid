130dc9011256bf295befbe8107e4bcfee251e8fa
platforms-quberneu <platforms@quberneu.com>
2025-12-03 19:47:25 +0530

    Added Terraform infra + workflows" (#19)
    * Added Terraform infra + workflows"
    
    * Update plan.yml
    
    * Update plan.yml
    
    * Directory change
    
    * bucket init for terraform
    
    * Tf format fix
    
    * tf format fix
    
    * tf format fix
    
    * terraform format fix
    
    * tf formatter tool used
    
    * Validation fix for terraform
    
    * tf variables
    
    * Finally validations
    
    * TF Apply workflows fix

---
+++ infra/terraform.tfvars
@@ -0,0 +1,3 @@
+project_id = "ultra-sunset-472113-j0"
+region     = "us-central1"
+zone       = "us-central1-a"
---
+++ infra/variables.tf
@@ -0,0 +1,28 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region where resources will be created"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "The GCP zone where the VM will be created"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "network_name" {
+  description = "Name of the VPC network"
+  type        = string
+  default     = "minimax-network"
+}
+
+variable "subnet_name" {
+  description = "Name of the subnetwork"
+  type        = string
+  default     = "minimax-subnet"
+}
